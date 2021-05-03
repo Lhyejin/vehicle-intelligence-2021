59eef899a5bc96f69bc502bd238f706c1d05384f
Lhyejin <hub0913@kookmin.ac.kr>
2021-05-04 00:01:38 +0900

    Implement week5 assignment

--- code/week-5/README.md
+++ code/week-5/README.md
@@ -1,6 +1,33 @@
 # Week 5 - Path Planning & the A* Algorithm
 
 ---
+## Report
+
+5주차 과제의 목표는 방향 t를 고려한 optimal policy 찾을 수 있도록 구현하는 것이다.
+기존의 dynamic programming 방식에서 t를 고려하게 되면 action은 4방향이 아닌 (right, forward, left)로
+바뀌게 된다. 구현은 이 변한 action과 t를 고려하여, 기존 policy.py와 비슷한 방향으로 진행한다.
+
+먼저, optimum_policy_2D 함수를 실행하게 되면, value와 policy를 4방향에 대해서 초기화한다.
+그 뒤에, while문을 반복하면서 value function이 안정화될 때까지, 즉 change하지 않을 때까지 진행한다.
+처음에는 모든 value가 초기화 갚으로 되어있고, 각 방향 t에 대해서 goal 위치의 value 값만 0으로 설정되고, 그 뒤에 차례로 goal 주위부터 value가
+갱신되기 시작한다. 
+현재 위치 y,x,t에서 모든 action을 진행했을 때의 value를 구하여 현재 위치의 value를 갱신하거나 유지한다.
+다음 위치는 t에서 각 action을 진행하여 구한다. 맵의 절대적인 방향을 [(-1,  0),( 0, -1),( 1,  0),( 0,  1)]이라고 정의한다면, 
+t에 따라 left, right의 방향은 밑과 같이 변하게 된다.
+
+t=0, left=1, forward=0, right=3
+t=1, left=2, forward=1, right=0
+t=2, left=3, forward=2, right=1
+t=3, left=4, forward=3, right=2
+
+t를 기준으로 left, right는 주어진 action 변수 + t가 된다. (t+action)
+기존 policy.py와 다르게 다음 위치로 이동하게 되면 t 또한 변하게 되므로 같이 업데이트 시켜야 한다.
+나머지 구현방식은 모두 policy.py에 따른다.
+
+policy를 모두 갱신시킨 후에 start와 goal이 주어지면 optimal path를 찾을 수 있다.
+시작 위치에서부터 policy대로 계속 행동하면 goal이 나온다. 만약 policy대로 이동했는데도 goal이 나오지 않을 경우에는
+맵 위치를 벗어나게 되므로, 맵 이외의 위치가 나온다면 실패라고 구현한다.
+
 
 ## Examples
 
--- code/week-5/assignment.py
+++ code/week-5/assignment.py
@@ -70,16 +70,40 @@ def optimum_policy_2D(grid, init, goal, cost):
             # Mark the final state with a special value that we will
             # use in generating the final path policy.
             if (y, x) == goal and value[(t, y, x)] > 0:
-                # TODO: implement code.
-                pass
+                value[(t, y, x)] = 0
+                policy[(t, y, x)] = 0 # goal이기 때문에 뭘하든 상관없음
+                change = True
+
             # Try to use simple arithmetic to capture state transitions.
             elif grid[(y, x)] == 0:
-                # TODO: implement code.
-                pass
+                for act in action: # 모든 action에 대해서 수행
+                    t2, y2, x2 = (t+act)%4, y + forward[(t+act)%4][0], x + forward[(t+act)%4][1] # 다음 위치 갱신
+                    if 0 <= y2 < grid.shape[0] and 0 <= x2 < grid.shape[1] \
+                        and grid[(y2, x2)] == 0:
+                        v2 = value[(t2, y2, x2)] + cost[act+1]
+                        if v2 < value[(t, y, x)]: #현재 value가 v2보다 클 때, v2로 update
+                            change = True
+                            value[(t, y, x)] = v2
+                            policy[(t, y, x)] = act
+
+
     # Now navigate through the policy table to generate a
     # sequence of actions to take to follow the optimal path.
-    # TODO: implement code.
-
+    # TODO: implement code
+    # init 이용해서 optimal path 출력 (policy2D에)
+    curr_y, curr_x, curr_t = init[0], init[1], init[2]
+    fail = False
+    while True:
+        if (curr_y, curr_x) == goal: # goal에 도달할 떄까지 반복한다.
+            policy2D[(curr_y, curr_x)] = '*'
+            break
+        act = policy[(curr_t, curr_y, curr_x)]
+        policy2D[(curr_y, curr_x)] = action_name[act+1] # action 이름 할당
+        curr_t, curr_y, curr_x = (curr_t+act)%4, curr_y + forward[(curr_t+act)%4][0], curr_x + forward[(curr_t+act)%4][1]
+        if grid.shape[0] <= curr_y or curr_y < 0 or 0 > curr_x or curr_x >= grid.shape[1]: # 맵외로 벗어나면 실패
+            # 길이 없음, 실패
+            fail = True
+            break
     # Return the optimum policy generated above.
     return policy2D
 
